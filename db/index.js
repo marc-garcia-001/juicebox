24e4c4f037be5bba43d9be8a4527db733bf8be06
Marc Garcia <garcia.marc.001@gmail.com>
2021-10-10 11:38:42 -0400

    connection line

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 
 const client = new Client(
-  process.env.DATABASE_URL || { user: "postgres", password: "Oppa0318?" }
+  process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev'
 );
 
 async function createUser({ username, password, name, location }) {
